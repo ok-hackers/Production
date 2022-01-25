d6f4f105da4d4b2b98d0408858c1cee87e44f0cb
natefabian18 <natefabian18@gmail.com>
2022-01-24 19:32:17 -0500

    implementations and template files for Hamburger menu and wider admin screens OKH-226/OKH-229

---
+++ src/Comps/AdminHamburgerMenu.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    let HamburgerExspanded = false;
+
+    function exspandHamburger() {
+        HamburgerExspanded = !HamburgerExspanded;
+    }
+</script>
+
+<div id="HamburgerContainer" class:expand={HamburgerExspanded} aria-label="HamburgerMenu">
+    <div class="HamburgerButton">
+        <img src="/MaterialDesignIcons/menu.svg" alt="Hamburger" on:click={exspandHamburger} aria-label="ExpandHamburgerMenu">
+        <p>Collapse Menu</p>
+    </div>
+</div>
+
+<style>
+    #HamburgerContainer {
+        position: absolute;
+        background-color: green;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        width: 3em;
+        overflow-x: hidden;
+    }
+
+    .expand {
+        overflow-x: visible !important;
+        width: max-content !important;
+    }
+
+    .HamburgerButton {
+        cursor: pointer;
+        display: flex;
+    }
+
+    .HamburgerButton > img {
+        width: 3em;
+        height: 3em;
+    }
+
+    .HamburgerButton > p {
+        align-self: center;
+        margin: 0 0.5em;
+    }
+</style>
---
+++ src/routes/Admin/__layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import HamburgerMenu from "../../Comps/AdminHamburgerMenu.svelte";
+</script>
+
+<div id="AdminHamburgerLayer">
+    <nav>
+        <!-- Root Navmenu will be here -->
+    </nav>
+    <div id="ContentHolder">
+        <svelte:component this={HamburgerMenu} />
+        <div id="SlotHolder">
+            <slot></slot>
+        </div>
+    </div>
+</div>
+
+<style>
+    nav {
+        height: 3em;
+        background-color: lightgrey;
+    }
+
+    #ContentHolder {
+        position: relative;
+    }
+
+    #SlotHolder {
+        margin-left: 3em;
+        min-height: calc(100vh - 3em);
+    }
+</style>
