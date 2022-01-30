36f26c6eb379ab8c4e4885850089df3c73eb83fd
josh secrist <secrjosh@gmail.com>
2022-01-30 12:16:14 -0500

    user Settings page complete, one test pending, also pending for authentication code for username change

---
+++ src/routes/APIs/UserSettings/[newUser]-[currentUser]-[auth].ts
@@ -0,0 +1,44 @@
+import Database, {DBGroups} from "../../../Lib/DatabaseConnector"
+import { getDatabase, ref, set } from "firebase/database";
+
+
+export async function get({ params }) {
+    let {newUser} = params;
+    let {currentUser} = params;
+    let {auth} = params;
+
+    let ReturnOBJ = DatabaseUpdate(newUser, currentUser);
+    return ReturnOBJ;
+}
+
+async function DatabaseUpdate(newUser: string, currentUser: string) {
+    let db = new Database(DBGroups.Users);
+    let DatabaseData = await db.data;
+    let arrayNames = Object.keys(DatabaseData);
+    let namematch = arrayNames.findIndex((name)=>{
+        return name == currentUser;
+    });
+
+    let activeUser = DatabaseData[currentUser];
+    
+    let firstname = newUser.split(" ")[0];
+    let lastname = newUser.split(" ")[1];
+       
+    activeUser.fname = firstname;
+    activeUser.lname = lastname;
+    if (namematch >= 0) {
+        set(ref(db.database, 'users/' + currentUser), activeUser)
+        return {
+            body: {
+                data: "nameChanged"
+            }
+        }
+    }
+    else{
+        return {
+            body: {
+              data: 404  
+            }
+        }
+    }
+}
