9ec5072bb636c088ad6313d9647f54bffe0f0ae7
EncryptedMindz <85453518+EncryptedMindz@users.noreply.github.com>
2022-02-08 19:15:26 -0500

    Rewrote index and login tests with Cypress

--- cypress/integration/User/index.spec.ts
+++ cypress/integration/User/index.spec.ts
@@ -1,13 +1,14 @@
 describe('Homepage Test', ()=>{
     it('test home page', ()=>{
-        cy.visit('http://localhost:3000/'); //visit base site
+        cy.visit('http://localhost:3000/login'); //visit login page
         cy.get('#email').type('lane.wilkerson@stvincent.edu')
         cy.get('#password').type('testing123')
-        cy.get('')
+        cy.get('#quickstart-sign-in').click() //signing user in
+        cy.get('#NaviagateUser').click() //going to user home page
         
-        cy.get('h1').then(($heading)=>{
-            const txt = $heading.text();
-            expect(txt).toEqual("Welcome to SvelteKit")
+        cy.get('title').then(($title)=>{
+            const txt = $title.text();
+            expect(txt).toEqual("SVC HackLabs Login Page")
         })
         
     })
