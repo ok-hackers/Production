87eee4e1b09f9c393a387096cb81ff658b96b4bb
SwickJ94 <jacob.swick@stvincent.edu>
2022-02-10 16:34:41 -0500

    2nd commit revision for buttons

---
+++ src/routes/Admin/ManageUsersPage.svelte
@@ -0,0 +1,43 @@
+<!-- this code is my original code. Putting this here to have it up and then will continue working on it after.-->
+<svelte:head>
+  <title>SVC HackLabs Manage Users Page</title>
+
+  
+  <!-- <link rel="stylesheet" href="https://code.getmdl.io/1.1.3/material.orange-indigo.min.css"> -->
+  <link rel="stylesheet" href="/src/style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <script defer src="https://code.getmdl.io/1.1.3/material.min.js"></script>
+
+</svelte:head>
+<!-- 
+  <script>
+  //This data is all dummy data. I do not know how to make the database usernames sync with my code. - Jake
+	let fname1 = 'nate';
+	let lname1 = 'fabian';
+  let username1 = "nate.fabian@stvincent.edu";
+
+  let fname2 = 'josh';
+	let lname2 = 'secrist';
+  let username2 = "joshua.secrist@stvincent.edu";
+  
+  let fname3 = 'jake';
+	let lname3 = 'swick';
+  let username3 = "jacob.swick@stvincent.edu";
+	
+  let fname4 = 'lane';
+	let lname4 = 'wilkerson';
+  let username4 = "lane.wilkerson@stvincent.edu";
+	
+  
+  let group = 'None';
+</script>
+
+-->
+<!-- New User Button-->
+<a href = "New_User_Page" aria-label = "New User" > NewUser </a> 
+
+<!-- Edit User1-->
+<a href = "Edit_User" aria-label = "Edit" > editUser </a> 
+
+<!-- Delete User-->
+<a href = "Delete_User" aria-label = "Delete" > delUser </a> 
