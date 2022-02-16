7980019d6a8f3679bc9380781064c7a488602501
Nate Fabian <natefabian18@gmail.com>
2022-02-15 20:40:16 -0500

    sped up login as admin to make tests run faster

--- cypress/integration/Admin/logInAsAdmin.ts
+++ cypress/integration/Admin/logInAsAdmin.ts
@@ -1,10 +1,10 @@
 export default function LoginAsAdmin() {
     cy.visit('http://localhost:3000/');
     cy.get('#TempLoginButton').click();
-    cy.wait(1000);
+    cy.wait(200);
     cy.get('#email').type('nathan.fabian@stvincent.edu');
     cy.get('#password').type('testing');
-    cy.wait(1000);
+    cy.wait(200);
     cy.get('#quickstart-sign-in').click();
     cy.get('#NavigateAdmin').click();
 }
