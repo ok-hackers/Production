96e206ee6003be62a1b7f222081b7aed84f83957
Jake Swick <jacob.swick@stvincent.edu>
2022-03-05 13:42:37 -0500

    Latest

--- src/routes/User/availableLabs.svelte
+++ src/routes/User/availableLabs.svelte
@@ -5,16 +5,47 @@
     import { goto } from "$app/navigation";
 
     let labs:Array<any> = null
-    let searchQuery = null;
-    let labName = null;
+    let searchQuery = null
+    let labName = null
+    let labStatus = null
+    let labKeys:Array<any> = null
+    let labArray:Array<any> = []
+    let publishedLabs:Array<any> = []
+    let unpublishedLabs:Array<any> = []
 
     //function will get the lab data for the user to view them
     async function grabLabData() { //used Lanes function as it does the same thing I would have written
         let response = await fetch('/APIs/Labs/getAllLabMetaData');
         let getAllLabMetaData = await response.json();        
-        labs = Object.keys(getAllLabMetaData.data);
+        if (getAllLabMetaData.status == 200) {
+                labKeys = Object.keys(getAllLabMetaData.data)
+                for (let i = 0; i < labKeys.length; i++){
+                    labArray.push(getAllLabMetaData.data[labKeys[i]])
+                }
+                labArray = labArray
+            }
+            else {
+                alert('No labs available at this moment. Ask Dr. Serapiglia to upload some! :D');
+            }
+        labArray = labArray
+        sortLabs()
     }
     grabLabData();
+
+    //Takes all the labs and creates new arrays for them based on whether they are published or not
+    async function sortLabs() {
+        labArray = labArray
+        for (let i = 0; i < labArray.length; i++){
+            if (labArray[i].Published == 1) {
+                publishedLabs.push(labKeys[i])
+            }
+            else {
+                unpublishedLabs.push(labKeys[i])
+            }
+        }
+        publishedLabs = publishedLabs
+        unpublishedLabs = unpublishedLabs
+    }
 
     function startLab(openLab) { 
      goto(`/User/openLab-${openLab}`) //page coming soon
@@ -24,6 +55,11 @@
      goto(`/User/progressReport-${labPerformance}`) //page coming soon
     }
 
+    /* async function labStatus(status){
+      if (status = 0){
+        
+      }
+    } */
 
     //sorts labs based on search query
     function searchfunc() {
@@ -47,15 +83,17 @@
         <div class="searchBar">
             <input id="searchBar" placeholder="Lab Name" aria-label="Search Bar" bind:value={searchQuery} on:change={searchfunc}/>
         </div>
-        <div class="grey">
-        </div>
+        <div class="grey"></div>
         <h1>Available Labs</h1>
-        {#if labs != null}
-        {#each labs as lab, i}
+        {#if publishedLabs != null}
+        {#each publishedLabs as lab, i}
           <div class = "displayLabs">
             <h2 class="labName{i}">{lab}</h2>
             <button  on:click={() => {startLab(lab)}} class="button button--raised start" id="openLab{i}" name="openLab">Open Lab</button>
             <button on:click={() => {labResults(lab)}} class="button button--raised results" id="performanceResults{i}" name="performanceResults">Performance Report</button>
+            <div class = "status">
+              <!-- <div>{lab}</div> -->
+              </div>
           </div> 
         {/each}
       {/if} 
@@ -135,5 +173,10 @@
         text-align: center;
         height: 60px;
         background-color: rgba(214, 214, 214) !important;
+    }
+    .status{
+      left: 10px;
+      top: 14px;
+      
     }
 </style>
