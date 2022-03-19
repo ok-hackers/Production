c4e268b05895d0baa718c535c5c0e20e2f31e6cf
Jake Swick <jacob.swick@stvincent.edu>
2022-03-18 20:23:37 -0400

    Start of gradesPage

---
+++ src/routes/Admin/gradeList.svelte
@@ -0,0 +1,111 @@
+// Written by Jake Swick
+
+<script lang="ts">
+
+</script>
+
+
+<main>
+    <div id="main">
+        <div class="grey">
+            <h1>Lab Name</h1>
+        </div>
+        <h1>Complete</h1>
+        
+        {#if publishedLabs != null}
+            {#each publishedLabs as lab, i}
+            <div class = "displayLabs">
+                <h2 class="labName{i}">{lab}</h2>
+                <button  on:click={() => {unpublishLab(lab)}} class="button button--raised review" id="unpublishLab{i}" name="review">Review</button>
+            </div> 
+            {/each}
+        {/if} 
+        <br><br>
+        <h1>Incomplete</h1>
+        {#if unpublishedLabs != null}
+            {#each unpublishedLabs as lab, i}
+            {/each}
+        {/if} 
+    </div>
+</main>
+
+<style>
+.displayLabs {
+        position: relative;
+        border-radius: 8px;
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 95%;
+        height: 70px;
+        background-color: rgb(197, 196, 196);
+        margin-bottom: 10px;
+    }
+  .searchBar {
+        text-align: right;
+        margin-right: 30px;
+        margin-top: 15px;
+    }
+    h1 {
+        color: #008000;
+        font-size: 28px;
+        text-align:left;
+        margin-left: 15px;
+        margin-bottom: 10px;
+        font-weight: bold;
+    }
+    h2 {
+        position: absolute;
+        left: 10px;
+        top: 14px;
+        color: black;
+    }
+    #startLab {
+        height: 50px;
+        width: 120px;
+        font-size: 15px;
+        color: black;
+        background-color: white;
+        position: absolute;
+        right: 10px;
+        top: 10px;
+    }
+    #performanceResults {
+        height: 50px;
+        font-size: 15px;
+        color: white;
+        background-color: red;
+        position: absolute;
+        right: 140px;
+        top: 10px;
+    }  
+    .start {
+        height: 50px;
+        width: 120px;
+        font-size: 15px;
+        color: black;
+        background-color: white;
+        position: absolute;
+        right: 140px;
+        top: 10px;
+    }  
+    .results {
+        height: 50px;
+        width: 120px;
+        font-size: 15px;
+        color: black;
+        background-color: white;
+        position: absolute;
+        right: 10px;
+        top: 10px;
+    }
+    .grey {
+        text-align: center;
+        height: 60px;
+        background-color: rgba(214, 214, 214) !important;
+    }
+    .status{
+      left: 10px;
+      top: 14px;
+      
+    }
+</style>
--- src/routes/Admin/index.svelte
+++ src/routes/Admin/index.svelte
@@ -7,8 +7,9 @@
 	<a id="NavigateLabs" href="/Admin/labs">Labs |</a>
 	<a id="ManageGroups" href="/Admin/managegroups">Manage Groups |</a>
 	<a id="ManageUsersPage" href="/Admin/manageUsersPage">Manage Users |</a>
-	<a id="CreateUserPage" href="/Admin/createUser">Create User</a>
-	<a href="/Admin/editUser-aCypressTest">Edit User</a>
+	<a id="CreateUserPage" href="/Admin/createUser">Create User |</a>
+	<a href="/Admin/editUser-aCypressTest">Edit User |</a>
+	<a id="Grades" href="/Admin/gradeList">Grade List </a>
 </main>
 
 <style></style>
